361b890e9fcebd213570c4a638f8478a6f41e0c7
Paul <mokrenko51@gmail.com>
2024-10-18 21:42:04 +0300

    ~ set up basic logic on users and team

--- src/tasks/entity/task.entity.ts
+++ src/tasks/entity/task.entity.ts
@@ -1,7 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-
-
 import { User } from '../../users/entity/user.entity';
 import { Team } from '../../Teams/entity/team.entity';
 
